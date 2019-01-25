f395edf7b19cac11ec638ad831328b17df116929
BaiCai <709931138@qq.com>
2019-01-25 13:52:45 +0800

    Redis部分优化

--- src/main/java/com/d2c/shop/modules/security/service/impl/UserServiceImpl.java
+++ src/main/java/com/d2c/shop/modules/security/service/impl/UserServiceImpl.java
@@ -9,11 +9,10 @@ import com.d2c.shop.modules.security.model.UserDO;
 import com.d2c.shop.modules.security.service.RoleService;
 import com.d2c.shop.modules.security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author BaiCai
@@ -23,27 +22,16 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
 
     @Autowired
     private RoleService roleService;
-    @Autowired
-    private RedisTemplate<String, UserDO> redisTemplate;
 
     @Override
+    @Cacheable(value = "USER", key = "'findByUsername:'+#username", unless = "#result == null")
     public UserDO findByUsername(String username) {
-        UserDO user = null;
-        String key = "USER::findByUsername:" + username;
-        UserDO cache = redisTemplate.opsForValue().get(key);
-        if (cache != null) {
-            user = cache;
-        } else {
-            Wrapper<UserDO> queryWrapper = new QueryWrapper();
-            ((QueryWrapper<UserDO>) queryWrapper).eq("username", username);
-            user = this.getOne(queryWrapper);
-        }
+        Wrapper<UserDO> queryWrapper = new QueryWrapper();
+        ((QueryWrapper<UserDO>) queryWrapper).eq("username", username);
+        UserDO user = this.getOne(queryWrapper);
         if (user == null) return null;
         List<RoleDO> roles = roleService.findByUserId(user.getId());
         user.setRoles(roles);
-        if (cache == null) {
-            redisTemplate.opsForValue().set(key, user, 1, TimeUnit.DAYS);
-        }
         return user;
     }
 
